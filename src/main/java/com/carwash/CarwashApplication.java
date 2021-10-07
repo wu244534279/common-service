56a2efb86e5491967e2a69f5b16e4f77d928ad5a
wu244534279 <244534279@qq.com>
2021-10-07 13:19:41 +0800

    initial project

---
+++ src/main/java/com/carwash/CarwashApplication.java
@@ -0,0 +1,13 @@
+package com.carwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashApplication.class, args);
+	}
+	
+}
